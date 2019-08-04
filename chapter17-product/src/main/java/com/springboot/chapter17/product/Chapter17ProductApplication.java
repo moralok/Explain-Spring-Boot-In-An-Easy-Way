b8e1c97c4a9af8bc80dd0df28438cb1e85eb8afd
moralok <moralok@163.com>
2019-08-04 23:55:37 +0800

    引入 Eureka 的服务发现依赖，搭建多节点为服务

---
+++ chapter17-product/src/main/java/com/springboot/chapter17/product/Chapter17ProductApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.chapter17.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter17ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter17ProductApplication.class, args);
+    }
+
+}
---
+++ chapter17-user/src/main/java/com/springboot/chapter17/user/Chapter17UserApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.chapter17.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter17UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter17UserApplication.class, args);
+    }
+
+}
