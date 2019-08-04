09d02b2be9b3253646f695d3df5a35534d377c13
moralok <moralok@163.com>
2019-08-04 23:39:25 +0800

    整合 Spring Cloud，使用 Eureka 作为服务治理器

---
+++ chapter17-server/src/main/java/com/springboot/chapter17/server/Chapter17ServerApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.chapter17.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Chapter17ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter17ServerApplication.class, args);
+    }
+
+}
